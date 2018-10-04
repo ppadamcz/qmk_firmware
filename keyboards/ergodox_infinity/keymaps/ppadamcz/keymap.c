067eb8c688453a40d31f0a5f34c68b7468df2424
Paweł Adamczyk <pawel.p.adamczyk@intel.com>
2018-10-04 19:32:58 +0200

    Make lower row 4 buttons with 2 modifiers

--- keyboards/ergodox_infinity/keymaps/ppadamcz/keymap.c
+++ keyboards/ergodox_infinity/keymaps/ppadamcz/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |Z/LSht|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//RSft|        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | ~L1  |   ~  |   \| |  =   | LGui |                                       |  '"  |  -   |   [  |   ]  | ~L1  |
+ *   | ~L1  | ~L1  |   =  |  TAB | LGui |                                       |  ~L2 |  '"  |  -   | ~L1  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Ctrl |RESET |       |RESET |Ctrl/Esc|
@@ -44,17 +44,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_NO,       KC_Q,          KC_W,     KC_E,      KC_R,      KC_T,   KC_NO,
           KC_NO,       KC_A,          KC_S,     KC_D,      KC_F,      KC_G,
           KC_NO,       LSFT_T(KC_Z),  KC_X,     KC_C,      KC_V,      KC_B,   KC_NO,
-                       KC_FN1,        KC_GRV,   KC_BSLS,   KC_EQL,    KC_LGUI,
+                       KC_FN1,        KC_FN1,   KC_EQL,    KC_TAB,    KC_LGUI,
 
                                                                KC_LCTRL,   RESET,
                                                                            KC_HOME,
                                                 CTL_T(KC_ESC), KC_BSPC,    KC_END,
         // right hand
-        KC_NO,       KC_6,     KC_7,     KC_8,      KC_9,      KC_0,             KC_NO,
-        KC_NO,       KC_Y,     KC_U,     KC_I,      KC_O,      KC_P,             KC_NO,
-                     KC_H,     KC_J,     KC_K,      KC_L,      LT(MDIA,   KC_SCLN),         KC_NO,
-        KC_NO,       KC_N,     KC_M,     KC_COMM,   KC_DOT,    RSFT_T(KC_SLSH),  KC_NO,
-                       KC_QUOT,  KC_QUOT,  KC_MINS,   KC_RBRC,   KC_FN1,
+        KC_NO,       KC_6,     KC_7,     KC_8,      KC_9,      KC_0,                KC_NO,
+        KC_NO,       KC_Y,     KC_U,     KC_I,      KC_O,      KC_P,                KC_NO,
+                     KC_H,     KC_J,     KC_K,      KC_L,      LT(MDIA,   KC_SCLN), KC_NO,
+        KC_NO,       KC_N,     KC_M,     KC_COMM,   KC_DOT,    RSFT_T(KC_SLSH),     KC_NO,
+                     KC_FN2,   KC_QUOT,  KC_MINS,   KC_FN1,    KC_FN1,
 
         RESET,        CTL_T(KC_ESC),
         KC_PGUP,
